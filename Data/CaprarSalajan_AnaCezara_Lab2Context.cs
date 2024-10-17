9efeefe8ca70d4d89cba3d700e9db3534fb2eb77
ANA-CEZARA CĂPRAR-SĂLĂJAN <ana.caprar@stud.ubbcluj.ro>
2024-10-17 18:27:45 +0300

    Add project files.

---
+++ Data/CaprarSalajan_AnaCezara_Lab2Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CaprarSalajan_AnaCezara_Lab2.Models;
+
+namespace CaprarSalajan_AnaCezara_Lab2.Data
+{
+    public class CaprarSalajan_AnaCezara_Lab2Context : DbContext
+    {
+        public CaprarSalajan_AnaCezara_Lab2Context (DbContextOptions<CaprarSalajan_AnaCezara_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CaprarSalajan_AnaCezara_Lab2.Models.Book> Book { get; set; } = default!;
+        public DbSet<CaprarSalajan_AnaCezara_Lab2.Models.Publisher> Publisher { get; set; } = default!;
+        public DbSet<CaprarSalajan_AnaCezara_Lab2.Models.Author> Author { get; set; } = default!;
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,16 @@
+﻿namespace CaprarSalajan_AnaCezara_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName{ get; set; }
+        public string LastName { get; set; }
+        public ICollection<Book>? Books { get; set; }
+
+        public string AuthorFullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+    }
+}
