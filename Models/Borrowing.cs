21979af00d57762dcb74a6ab02e50ad96769c346
ANA-CEZARA CĂPRAR-SĂLĂJAN <ana.caprar@stud.ubbcluj.ro>
2024-11-08 14:42:28 +0200

    laborator 5

--- Models/Borrowing.cs
+++ Models/Borrowing.cs
@@ -5,8 +5,12 @@ namespace CaprarSalajan_AnaCezara_Lab2.Models
     public class Borrowing
     {
         public int ID { get; set; }
+
+        [Display(Name = "Member")]
         public int? MemberID { get; set; }
         public Member? Member { get; set; }
+
+        [Display(Name = "Book")]
         public int? BookID { get; set; }
         public Book? Book { get; set; }
         [DataType(DataType.Date)] 
